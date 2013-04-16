5054cd2706743f6daf3793c210531904731cb4a3
Boris Minaev <mb.787788@gmail.com>
2013-04-16 21:15:14 +0400

    add all methods of IabHelper to OpenIabHelper

--- samples/life_openiab/src/org/onepf/life2/oms/OpenIabHelper.java
+++ samples/life_openiab/src/org/onepf/life2/oms/OpenIabHelper.java
@@ -3,15 +3,20 @@ package org.onepf.life2.oms;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
 import android.util.Log;
 import org.onepf.life2.oms.appstore.GooglePlayBillingService;
-import org.onepf.life2.oms.appstore.googleUtils.IabHelper;
+import org.onepf.life2.oms.appstore.googleUtils.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * User: Boris Minaev
  * Date: 16.04.13
  * Time: 16:42
  */
+
 public class OpenIabHelper {
     Context mContext;
     AppstoreServiceManager mServiceManager;
@@ -21,6 +26,9 @@ public class OpenIabHelper {
     // Is debug logging enabled?
     boolean mDebugLog = false;
     String mDebugTag = "IabHelper";
+
+    // Is setup done?
+    boolean mSetupDone = false;
 
     // Is an asynchronous operation in progress?
     // (only one at a time can be in progress)
@@ -39,6 +47,9 @@ public class OpenIabHelper {
     // Item types
     public static final String ITEM_TYPE_INAPP = "inapp";
     public static final String ITEM_TYPE_SUBS = "subs";
+
+    // Billing response codes
+    public static final int BILLING_RESPONSE_RESULT_OK = 0;
 
     public OpenIabHelper(Context context, String googlePublicKey) {
         mContext = context;
@@ -90,6 +101,125 @@ public class OpenIabHelper {
         return mAppstoreBillingService.handleActivityResult(requestCode, resultCode, data);
     }
 
+    public Inventory queryInventory(boolean querySkuDetails, List<String> moreSkus) throws IabException {
+        return queryInventory(querySkuDetails, moreSkus, null);
+    }
+
+    public Inventory queryInventory(boolean querySkuDetails, List<String> moreItemSkus,
+                                    List<String> moreSubsSkus) throws IabException {
+        return mAppstoreBillingService.queryInventory(querySkuDetails, moreItemSkus, moreSubsSkus);
+    }
+
+    public void queryInventoryAsync(final boolean querySkuDetails,
+                                    final List<String> moreSkus,
+                                    final IabHelper.QueryInventoryFinishedListener listener) {
+        final Handler handler = new Handler();
+        checkSetupDone("queryInventory");
+        flagStartAsync("refresh inventory");
+        (new Thread(new Runnable() {
+            public void run() {
+                IabResult result = new IabResult(BILLING_RESPONSE_RESULT_OK, "Inventory refresh successful.");
+                Inventory inv = null;
+                try {
+                    inv = queryInventory(querySkuDetails, moreSkus);
+                } catch (IabException ex) {
+                    result = ex.getResult();
+                }
+
+                flagEndAsync();
+
+                final IabResult result_f = result;
+                final Inventory inv_f = inv;
+                handler.post(new Runnable() {
+                    public void run() {
+                        listener.onQueryInventoryFinished(result_f, inv_f);
+                    }
+                });
+            }
+        })).start();
+    }
+
+    public void queryInventoryAsync(IabHelper.QueryInventoryFinishedListener listener) {
+        queryInventoryAsync(true, null, listener);
+    }
+
+    public void queryInventoryAsync(boolean querySkuDetails, IabHelper.QueryInventoryFinishedListener listener) {
+        queryInventoryAsync(querySkuDetails, null, listener);
+    }
+
+    public void consume(Purchase itemInfo) throws IabException {
+        mAppstoreBillingService.consume(itemInfo);
+    }
+
+    public void consumeAsync(Purchase purchase, IabHelper.OnConsumeFinishedListener listener) {
+        checkSetupDone("consume");
+        List<Purchase> purchases = new ArrayList<Purchase>();
+        purchases.add(purchase);
+        consumeAsyncInternal(purchases, listener, null);
+    }
+
+    public void consumeAsync(List<Purchase> purchases, IabHelper.OnConsumeMultiFinishedListener listener) {
+        checkSetupDone("consume");
+        consumeAsyncInternal(purchases, null, listener);
+    }
+
+    void consumeAsyncInternal(final List<Purchase> purchases,
+                              final IabHelper.OnConsumeFinishedListener singleListener,
+                              final IabHelper.OnConsumeMultiFinishedListener multiListener) {
+        final Handler handler = new Handler();
+        flagStartAsync("consume");
+        (new Thread(new Runnable() {
+            public void run() {
+                final List<IabResult> results = new ArrayList<IabResult>();
+                for (Purchase purchase : purchases) {
+                    try {
+                        consume(purchase);
+                        results.add(new IabResult(BILLING_RESPONSE_RESULT_OK, "Successful consume of sku " + purchase.getSku()));
+                    } catch (IabException ex) {
+                        results.add(ex.getResult());
+                    }
+                }
+
+                flagEndAsync();
+                if (singleListener != null) {
+                    handler.post(new Runnable() {
+                        public void run() {
+                            singleListener.onConsumeFinished(purchases.get(0), results.get(0));
+                        }
+                    });
+                }
+                if (multiListener != null) {
+                    handler.post(new Runnable() {
+                        public void run() {
+                            multiListener.onConsumeMultiFinished(purchases, results);
+                        }
+                    });
+                }
+            }
+        })).start();
+    }
+
+    // Checks that setup was done; if not, throws an exception.
+    void checkSetupDone(String operation) {
+        if (!mSetupDone) {
+            logError("Illegal state for operation (" + operation + "): IAB helper is not set up.");
+            throw new IllegalStateException("IAB helper is not set up. Can't perform operation: " + operation);
+        }
+    }
+
+    void flagStartAsync(String operation) {
+        if (mAsyncInProgress) throw new IllegalStateException("Can't start async operation (" +
+                operation + ") because another async operation(" + mAsyncOperation + ") is in progress.");
+        mAsyncOperation = operation;
+        mAsyncInProgress = true;
+        logDebug("Starting async operation: " + operation);
+    }
+
+    void flagEndAsync() {
+        logDebug("Ending async operation: " + mAsyncOperation);
+        mAsyncOperation = "";
+        mAsyncInProgress = false;
+    }
 
     void logDebug(String msg) {
         if (mDebugLog) Log.d(mDebugTag, msg);
