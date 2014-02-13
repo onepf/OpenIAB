a029f9afac07d5e37f060c9ecd173dd0674eca12
Oleg Orlov <oleg.orlov@onepf.org>
2014-02-13 18:41:13 +0400

    Fix for #125 comments about Amazon and Samsung don't need public keys

--- samples/trivialdrive/src/org/onepf/trivialdrive/MainActivity.java
+++ samples/trivialdrive/src/org/onepf/trivialdrive/MainActivity.java
@@ -177,6 +177,8 @@ public class MainActivity extends Activity {
         Log.d(TAG, "Creating IAB helper.");
         Map<String, String> storeKeys = new HashMap<String, String>();
         storeKeys.put(OpenIabHelper.NAME_GOOGLE, base64EncodedPublicKey);
+//      storeKeys.put(OpenIabHelper.NAME_AMAZON, "Unavailable. Amazon doesn't support RSA verification. So this mapping is not needed"); //
+//      storeKeys.put(OpenIabHelper.NAME_SAMSUNG,"Unavailable. SamsungApps doesn't support RSA verification. So this mapping is not needed"); //
         storeKeys.put("com.yandex.store", YANDEX_PUBLIC_KEY);
 
         mHelper = new OpenIabHelper(this, storeKeys);
