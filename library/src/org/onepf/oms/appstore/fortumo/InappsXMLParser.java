4eadc1cac4258eea73da17374a452d636744de40
akarimova <anastasiya.dremina@gmail.com>
2014-03-06 13:18:56 +0400

    renamed the constants: from smt_tag to tag_smt and from smt_attr to a attr_smt

--- library/src/org/onepf/oms/appstore/fortumo/InappsXMLParser.java
+++ library/src/org/onepf/oms/appstore/fortumo/InappsXMLParser.java
@@ -23,27 +23,27 @@ public class InappsXMLParser {
 
 
     //TAGS
-    private static final String INAPP_PRODUCTS_TAG = "inapp-products";
-    private static final String SUBSCRIPTIONS_TAG = "subscriptions";
-    private static final String SUBSCRIPTION_TAG = "subscription";
-    private static final String ITEMS_TAG = "items";
-    private static final String ITEM_TAG = "item";
-    private static final String SUMMARY_TAG = "summary";
-    private static final String SUMMARY_LOCALIZATION_TAG = "summary-localization";
-    private static final String SUMMARY_BASE_TAG = "summary-base";
-    private static final String PRICE_BASE_TAG = "price-base";
-    private static final String PRICE_LOCAL_TAG = "price-local";
-    private static final String COMMON_TITLE_TAG = "title";
-    private static final String COMMON_DESCRIPTION_TAG = "description";
-    private static final String PRICE_TAG = "price";
+    private static final String TAG_INAPP_PRODUCTS = "inapp-products";
+    private static final String TAG_SUBSCRIPTIONS = "subscriptions";
+    private static final String TAG_SUBSCRIPTION = "subscription";
+    private static final String TAG_ITEMS = "items";
+    private static final String TAG_ITEM = "item";
+    private static final String TAG_SUMMARY = "summary";
+    private static final String TAG_SUMMARY_LOCALIZATION = "summary-localization";
+    private static final String TAG_SUMMARY_BASE = "summary-base";
+    private static final String TAG_PRICE_BASE = "price-base";
+    private static final String TAG_PRICE_LOCAL = "price-local";
+    private static final String TAG_COMMON_TITLE = "title";
+    private static final String TAG_COMMON_DESCRIPTION = "description";
+    private static final String TAG_PRICE = "price";
 
     //ATTRS
-    private static final String PUBLISH_STATE_ATTR = "publish-state";
-    private static final String ID_ATTR = "id";
-    private static final String LOCALE_ATTR = "locale";
-    private static final String COUNTRY_ATTR = "country";
-    private static final String PERIOD_ATTR = "period";
-    private static final String AUTOFILL_ATTR = "autofill";
+    private static final String ATTR_PUBLISH_STATE = "publish-state";
+    private static final String ATTR_ID = "id";
+    private static final String ATTR_LOCALE = "locale";
+    private static final String ATTR_COUNTRY = "country";
+    private static final String ATTR_PERIOD = "period";
+    private static final String ATTR_AUTOFILL = "autofill";
 
 
     public Pair<List<InappBaseProduct>, List<InappSubscriptionProduct>> parse(Context context) throws XmlPullParserException, IOException {
@@ -78,24 +78,24 @@ public class InappsXMLParser {
             String tagName = parser.getName();
             switch (eventType) {
                 case XmlPullParser.START_TAG:
-                    if (tagName.equals(INAPP_PRODUCTS_TAG)) {
+                    if (tagName.equals(TAG_INAPP_PRODUCTS)) {
                         insideInapps = true;
-                    } else if (tagName.equals(ITEMS_TAG)) {
+                    } else if (tagName.equals(TAG_ITEMS)) {
                         if (!insideInapps) {
-                            inWrongNode(ITEMS_TAG, INAPP_PRODUCTS_TAG);
+                            inWrongNode(TAG_ITEMS, TAG_INAPP_PRODUCTS);
                         }
                         insideItems = true;
-                    } else if (tagName.equals(SUBSCRIPTIONS_TAG)) {
+                    } else if (tagName.equals(TAG_SUBSCRIPTIONS)) {
                         if (!insideInapps) {
-                            inWrongNode(SUBSCRIPTIONS_TAG, INAPP_PRODUCTS_TAG);
+                            inWrongNode(TAG_SUBSCRIPTIONS, TAG_INAPP_PRODUCTS);
                         }
                         insideSubs = true;
-                    } else if (tagName.equals(ITEM_TAG) || tagName.equals(SUBSCRIPTION_TAG)) {
-                        if (tagName.equals(SUBSCRIPTION_TAG)) {
+                    } else if (tagName.equals(TAG_ITEM) || tagName.equals(TAG_SUBSCRIPTION)) {
+                        if (tagName.equals(TAG_SUBSCRIPTION)) {
                             if (!insideSubs) {
-                                inWrongNode(SUBSCRIPTION_TAG, SUBSCRIPTIONS_TAG);
+                                inWrongNode(TAG_SUBSCRIPTION, TAG_SUBSCRIPTIONS);
                             }
-                            currentSubPeriod = parser.getAttributeValue(null, PERIOD_ATTR);
+                            currentSubPeriod = parser.getAttributeValue(null, ATTR_PERIOD);
                             if (!(InappSubscriptionProduct.ONE_MONTH.equals(currentSubPeriod) || InappSubscriptionProduct.ONE_YEAR.equals(currentSubPeriod))) {
                                 throw new IllegalStateException(String.format("Wrong \"period\" value: %s. Must be \"%s\" or \"%s\".", currentSubPeriod, InappSubscriptionProduct.ONE_MONTH,
                                         InappSubscriptionProduct.ONE_YEAR));
@@ -103,64 +103,64 @@ public class InappsXMLParser {
                             insideSub = true;
                         } else {
                             if (!insideItems) {
-                                inWrongNode(ITEMS_TAG, ITEMS_TAG);
+                                inWrongNode(TAG_ITEMS, TAG_ITEMS);
                             }
                             insideItem = true;
                         }
                         currentProduct = new InappBaseProduct();
-                        final String sku = parser.getAttributeValue(null, ID_ATTR);
+                        final String sku = parser.getAttributeValue(null, ATTR_ID);
                         final Matcher matcher = skuPattern.matcher(sku);
                         if (!matcher.matches()) {
                             throw new IllegalStateException(String.format("Wrong SKU ID: %s. SKU must match \"([a-z]|[0-9]){1}[a-z0-9._]*\"", sku));
                         }
                         currentProduct.setProductId(sku);
-                        final String publishState = parser.getAttributeValue(null, PUBLISH_STATE_ATTR);
+                        final String publishState = parser.getAttributeValue(null, ATTR_PUBLISH_STATE);
                         if (!(InappBaseProduct.UNPUBLISHED.equals(publishState) || InappBaseProduct.PUBLISHED.equals(publishState))) {
                             throw new IllegalStateException(String.format("Wrong publish state value: %s. Must be \"%s\" or \"%s\"", publishState, InappBaseProduct.UNPUBLISHED, InappBaseProduct.PUBLISHED));
                         }
                         currentProduct.setPublished(publishState);
-                    } else if (tagName.equals(SUMMARY_TAG)) {
+                    } else if (tagName.equals(TAG_SUMMARY)) {
                         if (!(insideItem || insideSub)) {
-                            inWrongNode(SUMMARY_TAG, ITEM_TAG, SUBSCRIPTION_TAG);
+                            inWrongNode(TAG_SUMMARY, TAG_ITEM, TAG_SUBSCRIPTION);
                         }
                         insideSummary = true;
-                    } else if (tagName.equals(SUMMARY_BASE_TAG)) {
+                    } else if (tagName.equals(TAG_SUMMARY_BASE)) {
                         if (!insideSummary) {
-                            inWrongNode(SUMMARY_BASE_TAG, SUMMARY_TAG);
+                            inWrongNode(TAG_SUMMARY_BASE, TAG_SUMMARY);
                         }
                         insideSummaryBase = true;
-                    } else if (tagName.equals(SUMMARY_LOCALIZATION_TAG)) {
+                    } else if (tagName.equals(TAG_SUMMARY_LOCALIZATION)) {
                         if (!insideSummary) {
-                            inWrongNode(SUMMARY_LOCALIZATION_TAG, SUMMARY_TAG);
+                            inWrongNode(TAG_SUMMARY_LOCALIZATION, TAG_SUMMARY);
                         }
-                        currentLocale = parser.getAttributeValue(null, LOCALE_ATTR);
+                        currentLocale = parser.getAttributeValue(null, ATTR_LOCALE);
                         if (!localePattern.matcher(currentLocale).matches()) {
                             throw new IllegalStateException(String.format("Wrong \"locale\" attribute value: %s. Must match [a-z][a-z]_[A-Z][A-Z].", currentCountryCode));
                         }
                         insideSummaryLocal = true;
-                    } else if (tagName.equals(COMMON_TITLE_TAG)) {
+                    } else if (tagName.equals(TAG_COMMON_TITLE)) {
                         if (!(insideSummaryBase || insideSummaryLocal)) {
-                            inWrongNode(COMMON_TITLE_TAG, SUMMARY_BASE_TAG, SUMMARY_LOCALIZATION_TAG);
+                            inWrongNode(TAG_COMMON_TITLE, TAG_SUMMARY_BASE, TAG_SUMMARY_LOCALIZATION);
                         }
-                    } else if (tagName.equals(COMMON_DESCRIPTION_TAG)) {
+                    } else if (tagName.equals(TAG_COMMON_DESCRIPTION)) {
                         if (!(insideSummaryBase || insideSummaryLocal)) {
-                            inWrongNode(COMMON_DESCRIPTION_TAG, SUMMARY_BASE_TAG, SUMMARY_LOCALIZATION_TAG);
+                            inWrongNode(TAG_COMMON_DESCRIPTION, TAG_SUMMARY_BASE, TAG_SUMMARY_LOCALIZATION);
                         }
-                    } else if (tagName.equals(PRICE_TAG)) {
+                    } else if (tagName.equals(TAG_PRICE)) {
                         if (!(insideItem || insideSub)) {
-                            inWrongNode(PRICE_TAG, ITEM_TAG, SUBSCRIPTION_TAG);
+                            inWrongNode(TAG_PRICE, TAG_ITEM, TAG_SUBSCRIPTION);
                         }
-                        currentProduct.setAutoFill(Boolean.parseBoolean(parser.getAttributeValue(null, AUTOFILL_ATTR)));
+                        currentProduct.setAutoFill(Boolean.parseBoolean(parser.getAttributeValue(null, ATTR_AUTOFILL)));
                         insidePrice = true;
-                    } else if (tagName.equals(PRICE_BASE_TAG)) {
+                    } else if (tagName.equals(TAG_PRICE_BASE)) {
                         if (!insidePrice) {
-                            inWrongNode(PRICE_BASE_TAG, PRICE_TAG);
+                            inWrongNode(TAG_PRICE_BASE, TAG_PRICE);
                         }
-                    } else if (tagName.equals(PRICE_LOCAL_TAG)) {
+                    } else if (tagName.equals(TAG_PRICE_LOCAL)) {
                         if (!insidePrice) {
-                            inWrongNode(PRICE_LOCAL_TAG, PRICE_TAG);
+                            inWrongNode(TAG_PRICE_LOCAL, TAG_PRICE);
                         }
-                        currentCountryCode = parser.getAttributeValue(null, COUNTRY_ATTR);
+                        currentCountryCode = parser.getAttributeValue(null, ATTR_COUNTRY);
                         final Matcher matcher = countryPattern.matcher(currentCountryCode);
                         if (!matcher.matches()) {
                             throw new IllegalStateException(String.format("Wrong \"country\" attribute value: %s. Must match [A-Z][A-Z].", currentCountryCode));
@@ -171,51 +171,51 @@ public class InappsXMLParser {
                     text = parser.getText();
                     break;
                 case XmlPullParser.END_TAG:
-                    if (tagName.equals(INAPP_PRODUCTS_TAG)) {
+                    if (tagName.equals(TAG_INAPP_PRODUCTS)) {
                         insideInapps = false;
-                    } else if (tagName.equals(ITEMS_TAG)) {
+                    } else if (tagName.equals(TAG_ITEMS)) {
                         insideItems = false;
-                    } else if (tagName.equals(SUBSCRIPTIONS_TAG)) {
+                    } else if (tagName.equals(TAG_SUBSCRIPTIONS)) {
                         insideSubs = false;
-                    } else if (tagName.equals(ITEM_TAG)) {
+                    } else if (tagName.equals(TAG_ITEM)) {
                         currentProduct.validateItem();
                         itemsList.add(currentProduct);
                         currentProduct = null;
-                    } else if (tagName.equals(SUBSCRIPTION_TAG)) {
+                    } else if (tagName.equals(TAG_SUBSCRIPTION)) {
                         InappSubscriptionProduct subscriptionProduct = new InappSubscriptionProduct(currentProduct, currentSubPeriod);
                         subscriptionProduct.validateItem();
                         subscriptionList.add(subscriptionProduct);
                         currentSubPeriod = null;
                         currentProduct = null;
                         insideSub = false;
-                    } else if (tagName.equals(SUMMARY_TAG)) {
+                    } else if (tagName.equals(TAG_SUMMARY)) {
                         insideSummary = false;
-                    } else if (tagName.equals(COMMON_TITLE_TAG)) {
+                    } else if (tagName.equals(TAG_COMMON_TITLE)) {
                         int length = text.length();
                         if (!(length >= 1 && length <= 55)) {
                             throw new IllegalStateException(String.format("Wrong title length: %d. Must be 1-55 symbols", length));
                         }
                         title = text;
-                    } else if (tagName.equals(COMMON_DESCRIPTION_TAG)) {
+                    } else if (tagName.equals(TAG_COMMON_DESCRIPTION)) {
                         int length = text.length();
                         if (!(length >= 1 && length <= 80)) {
                             throw new IllegalStateException(String.format("Wrong description length: %d. Must be 1-80 symbols", length));
                         }
                         description = text;
-                    } else if (tagName.equals(SUMMARY_BASE_TAG)) {
+                    } else if (tagName.equals(TAG_SUMMARY_BASE)) {
                         currentProduct.setBaseTitle(title);
                         currentProduct.setBaseDescription(description);
                         title = null;
                         description = null;
                         insideSummaryBase = false;
-                    } else if (tagName.equals(SUMMARY_LOCALIZATION_TAG)) {
+                    } else if (tagName.equals(TAG_SUMMARY_LOCALIZATION)) {
                         currentProduct.addTitleLocalization(currentLocale, title);
                         currentProduct.addDescriptionLocalization(currentLocale, description);
                         title = null;
                         description = null;
                         currentLocale = null;
                         insideSummaryLocal = false;
-                    } else if (tagName.equals(PRICE_BASE_TAG) || tagName.equals(PRICE_LOCAL_TAG)) {
+                    } else if (tagName.equals(TAG_PRICE_BASE) || tagName.equals(TAG_PRICE_LOCAL)) {
                         float price;
                         try {
                             price = Float.parseFloat(text);
@@ -223,13 +223,13 @@ public class InappsXMLParser {
                             throw new IllegalStateException(String.format("Wrong price: %s. Must be decimal.", text));
                         }
 
-                        if (tagName.equals(PRICE_BASE_TAG)) {
+                        if (tagName.equals(TAG_PRICE_BASE)) {
                             currentProduct.setBasePrice(price);
                         } else {
                             currentProduct.addCountryPrice(currentCountryCode, price);
                             currentCountryCode = null;
                         }
-                    } else if (tagName.equals(PRICE_TAG)) {
+                    } else if (tagName.equals(TAG_PRICE)) {
                         insidePrice = false;
                     }
                     break;
