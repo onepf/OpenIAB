a2afee6e42e013bfdd4b9e0cf6f8fa30630ef44b
akarimova <anastasiya.dremina@gmail.com>
2014-05-06 20:58:25 +0400

    fix #204

--- library/src/org/onepf/oms/appstore/TStoreRequestCallback.java
+++ library/src/org/onepf/oms/appstore/TStoreRequestCallback.java
@@ -23,12 +23,17 @@ import android.util.Log;
 import com.skplanet.dodo.IapPlugin;
 import com.skplanet.dodo.IapResponse;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.onepf.oms.OpenIabHelper;
 import org.onepf.oms.appstore.googleUtils.IabHelper;
 import org.onepf.oms.appstore.googleUtils.IabResult;
 import org.onepf.oms.appstore.googleUtils.Purchase;
-import org.onepf.oms.appstore.tstoreUtils.GsonConverter;
 import org.onepf.oms.appstore.tstoreUtils.Response;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Author: Ruslan Sayfutdinov
@@ -61,9 +66,9 @@ public class TStoreRequestCallback implements IapPlugin.RequestCallback {
             Log.e(TAG, "onResponse() response data is null");
             return;
         }
-        Response response = new GsonConverter().fromJson(data.getContentToString());
+        Response response = getResponse(data);
         Purchase purchase = new Purchase(OpenIabHelper.NAME_TSTORE);
-        IabResult result = null;
+        IabResult result;
         if (response.result.code.equals("0000")) {
             result = new IabResult(IabHelper.BILLING_RESPONSE_RESULT_OK, "Success");
             for (Response.Product product : response.result.product) {
@@ -76,6 +81,52 @@ public class TStoreRequestCallback implements IapPlugin.RequestCallback {
             result = new IabResult(IabHelper.BILLING_RESPONSE_RESULT_ERROR, response.result.message);
         }
         mListener.onIabPurchaseFinished(result, purchase);
+    }
+
+    private Response getResponse(IapResponse data) {
+        Response response = null;
+        try {
+            JSONObject jo = new JSONObject(data.getContentToString());
+            final String apiVersion = jo.optString("api_version");
+            final String identifier = jo.optString("identifier");
+            final String method = jo.getString("method");
+
+            JSONObject resultJsonObject = jo.getJSONObject("result");
+            final String message = resultJsonObject.optString("message");
+            final String code = resultJsonObject.optString("code");
+            final String txid = resultJsonObject.optString("txid");
+            final String receipt = resultJsonObject.optString("receipt");
+            final int count = resultJsonObject.optInt("count");
+
+            List<Response.Product> products = new ArrayList<Response.Product>();
+            if (resultJsonObject.has("product")) {
+                final JSONArray productJA = resultJsonObject.optJSONArray("product");
+                products = new ArrayList<Response.Product>();
+                for (int i = 0; i < productJA.length(); i++) {
+                    Response.Product product = new Response.Product();
+                    final JSONObject productJson = (JSONObject) productJA.get(i);
+                    product.appid = productJson.optString("appid");
+                    product.endDate = productJson.optString("endDate");
+                    product.id = productJson.optString("id");
+                    product.kind = productJson.optString("kind");
+                    product.name = productJson.optString("name");
+                    product.price = productJson.optDouble("price");
+                    product.purchasability = productJson.optBoolean("purchasability");
+                    product.startDate = productJson.optString("startDate");
+                    product.type = productJson.optString("type");
+                    product.validity = productJson.optInt("validity");
+                    final JSONObject statusJO = productJson.optJSONObject("status");
+                    product.status = new Response.Status(statusJO.optString("code"), statusJO.optString("message"));
+                    products.add(product);
+                }
+            }
+
+            Response.Result result = new Response.Result(code, message, txid, receipt, count, products);
+            response = new Response(apiVersion, identifier, method, result);
+        } catch (JSONException e) {
+            Log.e(TAG, "error during JSON parsing", e);
+        }
+        return response;
     }
 }
 
